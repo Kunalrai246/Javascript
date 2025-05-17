0712f89bbd401f593eed4696fa5a762b9173b713
Kunalrai246 <149142083+Kunalrai246@users.noreply.github.com>
2025-05-17 05:59:05 +0000

    lecture 7

---
+++ basics01/lec07.js
@@ -0,0 +1,18 @@
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2/22)
+// console.log(2**2)
+
+// let str1="kunal"
+// let str2="neelesh"
+// let str3=str1+str2
+// console.log(str3)
+
+console.log(1+"2")
+console.log(2+"1")
+console.log(1+2+"2")
