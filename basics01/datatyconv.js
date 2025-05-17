c338c635c082bdefa0c4f316bb96092b8840fdb5
Kunalrai246 <149142083+Kunalrai246@users.noreply.github.com>
2025-05-17 05:25:56 +0000

    datatypecoversion

---
+++ basics01/datatyconv.js
@@ -0,0 +1,24 @@
+// let score="33"
+// console.log(typeof(score))
+// console.log(score)
+// let scoreinnumber=Number(score)
+// console.log(typeof(scoreinnumber))
+
+// "33" jab ek number ko convert karte hai 33 eassily convert//
+// "33abc" not convert => value give NaN=Not an Number (type is NUMBER)//
+// "true"=>1; false=>0;
+//"1"=>true; 0=>false
+// ""=>false//
+//"neelesh"=>true//
+
+// now learn about cheize string ke andar kaise convert//
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let ans=1
+let boolinans=Boolean(ans)
+console.log(boolinans)
+// koi number ho ya koi string usko true krdenga
+// agar khali string ho ya zero ho to usse false;
